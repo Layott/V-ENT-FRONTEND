36fb0452dd79e4f6680379c56d843c0c7d5e2b10
Otavie <otavieokuoyo@gmail.com>
2024-09-09 14:15:05 +0100

    Added empty activity section and menu tabs at the top

---
+++ src/components/profile/activity/Activity.js
@@ -0,0 +1,11 @@
+import styles from './activity.module.css'
+
+const Activity = () => {
+  return (
+    <div className={styles.activityContainer}>
+        Work in Progress...
+    </div>
+  )
+}
+
+export default Activity
