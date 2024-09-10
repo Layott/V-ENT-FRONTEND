643f4c93fab9901ba2e6ccaeba1c05c93a86e537
Otavie <otavieokuoyo@gmail.com>
2024-09-10 08:36:26 +0100

    Add template for the tournament page

---
+++ src/components/tournaments/Tournaments.js
@@ -0,0 +1,11 @@
+import styles from './tournaments.module.css'
+
+const Tournaments = () => {
+  return (
+    <div className={styles.pageContainer}>
+
+    </div>
+  )
+}
+
+export default Tournaments
