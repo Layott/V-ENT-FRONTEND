d0039252b0bf66b220a38b055c478bef06166511
Otavie <otavieokuoyo@gmail.com>
2024-09-13 14:45:51 +0100

    Add template for edit-profile page

---
+++ src/app/edit-profile/page.js
@@ -0,0 +1,102 @@
+'use client'
+
+import { useState } from 'react';
+import { SlArrowRight } from "react-icons/sl";
+import Sidebar from '@/components/sidebar/Sidebar';
+import ProfileHeader from '@/components/profile/profile-header/ProfileHeader';
+import EditProfileDetails from '@/components/edit-profile/edit-profile-details/EditProfileDetails';
+import EditFavouriteGames from '@/components/edit-profile/edit-favourite-games/EditFavouriteGames';
+import EditGamingAccounts from '@/components/edit-profile/edit-gaming-accounts/EditGamingAccounts';
+import EditLinks from '@/components/edit-profile/edit-links/EditLinks';
+import styles from './profile.module.css'
+
+const EditProfile = () => {
+  const [activeTab, setActiveTab] = useState('edit-profile-details')
+
+  return (
+    <div className={styles.pageContainer}>
+      <ProfileHeader />
+      
+      <main className={styles.mainContainer}>
+        <Sidebar />
+      
+        <div className={styles.rightPaneEditProfileContainer}>
+
+        <div className={styles.menuContainer}>
+          <div className={styles.menuPanel}>
+            <div className={styles.nameMenu}>
+              <p>Menu</p>
+            </div>
+
+            <div className={styles.buttonContainer}>
+              <button
+                className={`${styles.tabBTN} ${activeTab === 'edit-profile-details' ? styles.activeTab : ''}`}
+                onClick={() => setActiveTab('edit-profile-details')}
+              >
+                Profile Info {activeTab === 'edit-profile-details' && <SlArrowRight />}
+              </button>
+
+              <button
+                className={`${styles.tabBTN} ${activeTab === 'favourite-games' ? styles.activeTab : ''}`}
+                onClick={() => setActiveTab('favourite-games')}
+              >
+                Favourite Games {activeTab === "favourite-games" && <SlArrowRight />}
+              </button>
+
+              <button
+                className={`${styles.tabBTN} ${activeTab === 'gaming-accounts' ? styles.activeTab : ''}`}
+                onClick={() => setActiveTab('gaming-accounts')}
+              >
+                Gaming Accounts {activeTab === "gaming-accounts" && <SlArrowRight />}
+              </button>
+
+              <button
+                className={`${styles.tabBTN} ${activeTab === 'web-social-links' ? styles.activeTab : ''}`}
+                onClick={() => setActiveTab('web-social-links')}
+              >
+                Web and Social Links {activeTab === "web-social-links" && <SlArrowRight />}
+              </button>
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+
+        <div className={styles.profileEditDashboard}>
+          {activeTab === 'edit-profile-details' && (
+            <div className={styles.editProfileDetailsContainer}>
+              <EditProfileDetails />
+            </div>            
+          )}
+
+          {activeTab === 'favourite-games' && (
+            <div className={styles.editFavouriteGamesContainer}>
+              <EditFavouriteGames />
+            </div>
+          )}
+
+          {activeTab === 'gaming-accounts' && (
+            <div className={styles.editGamingAccountsContainer}>
+              <EditGamingAccounts />
+            </div>
+          )}
+
+          {activeTab === 'web-social-links' && (
+            <div className={styles.editLinksContainer}>
+              <EditLinks />
+            </div>
+          )}
+        </div>
+      
+        </div>
+      
+      </main>
+
+    </div>
+  )
+}
+
+export default EditProfile
