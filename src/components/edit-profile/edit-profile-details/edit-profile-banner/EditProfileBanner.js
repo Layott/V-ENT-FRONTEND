22f47a4f230a40aded6fadbc978676da08c084f9
Otavie <otavieokuoyo@gmail.com>
2024-09-14 14:06:33 +0100

    Componentized parts of the EditProfileDetails

---
+++ src/components/edit-profile/edit-profile-details/edit-profile-banner/EditProfileBanner.js
@@ -0,0 +1,11 @@
+import styles from './edit-profile-banner.module.css'
+
+const EditProfileBanner = () => {
+  return (
+    <div className={styles.editProfileBannerContainer}>
+        EditProfileBanner
+    </div>
+  )
+}
+
+export default EditProfileBanner
