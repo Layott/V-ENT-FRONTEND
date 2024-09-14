d743a238f7f8a810d6f8772b7d950726f96bb91d
Otavie <otavieokuoyo@gmail.com>
2024-09-14 15:43:49 +0100

    Add the Edit Banner component

--- src/components/edit-profile/edit-profile-details/edit-profile-banner/EditProfileBanner.js
+++ src/components/edit-profile/edit-profile-details/edit-profile-banner/EditProfileBanner.js
@@ -1,9 +1,42 @@
+import Image from 'next/image'
+import { FiCamera } from 'react-icons/fi'
+import bannerImage from "@/images/profile_image_bg.webp"
+import bioStyles from "./../../../profile/profile-bio/profile-bio.module.css"
 import styles from './edit-profile-banner.module.css'
 
 const EditProfileBanner = () => {
+    const handleBannerUploader = (event) => {
+        const file = event.target.files[0]
+        if (file) {
+        console.log(`Banner uploaded: ${file.name}`)
+        // File upload logic
+        }
+    }
+
   return (
     <div className={styles.editProfileBannerContainer}>
-        EditProfileBanner
+        <div className={styles.editProfileBannerImageContainer}>
+            <Image
+                src={bannerImage}
+                alt='Banner to be Edited'
+            />
+        </div>
+        <p>We recommend an image that is 1256 x 256 px</p>
+
+
+        <div className={`${bioStyles.bannerUploader} ${styles.bannerUploader}`}>
+            <label htmlFor="bannerUpload" className={`${bioStyles.bannerUploadLabel} ${styles.bannerUploadLabel}`}>
+                <FiCamera className={bioStyles.uploadIcon} /> Change banner
+            </label>
+            <input
+                type="file"
+                accept="image/*"
+                onChange={handleBannerUploader}
+                id="bannerUpload"
+                className={bioStyles.uploadInput}
+            />
+        </div>
+
     </div>
   )
 }
