7d9448f092d87da7f691dcc23c147ce579c53d56
Otavie <otavieokuoyo@gmail.com>
2024-09-21 15:33:31 +0100

    Add all the templates for the Tournament Details Dashboard

---
+++ src/components/tournament-details/tournament-details-participants/TournamentDetailsParticipants.js
@@ -0,0 +1,11 @@
+import styles from './tournament-details-participants.module.css'
+
+const TournamentDetailsParticipants = () => {
+  return (
+    <div className={styles.tournamentDetailsParticipantsContainer}>
+        TournamentDetailsParticipants
+    </div>
+  )
+}
+
+export default TournamentDetailsParticipants
---
+++ src/components/tournament-details/tournament-details-prize/TournamentDetailsPrize.js
@@ -0,0 +1,11 @@
+import styles from './tournament-details-prize.module.css'
+
+const TournamentDetailsPrize = () => {
+  return (
+    <div className={styles.tournamentDetailsPrizeContainer}>
+        TournamentDetailsPrize
+    </div>
+  )
+}
+
+export default TournamentDetailsPrize
