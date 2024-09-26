118fc6b8305fdfe2f267a1124c0ae6b6219af29c
Otavie <otavieokuoyo@gmail.com>
2024-09-26 06:27:12 +0100

    Add templates for tournament-details-prize section

---
+++ src/components/tournament-details/tournament-details-prize/tournamentResults.js
@@ -0,0 +1,27 @@
+export const tournamentResults = [
+  {
+    position: "Winner (First Place)",
+    prize: "N1,000,000",
+    bonus: "1000 V-Coins",
+  },
+  {
+    position: "Second Place",
+    prize: "N750,000",
+    bonus: "500 V-Coins",
+  },
+  {
+    position: "Third Place",
+    prize: "N600,000",
+    bonus: "200 V-Coins",
+  },
+  {
+    position: "Fourth Place",
+    prize: "N500,000",
+    bonus: "None",
+  },
+  {
+    position: "Fifth Place",
+    prize: "N450,000",
+    bonus: "None",
+  },
+];
