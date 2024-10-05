0a1113ff709f9e9b8dc26b0ec65268076ad4a3a9
Otavie <otavieokuoyo@gmail.com>
2024-10-05 09:51:16 +0100

    Allow all field view or less field view on participants tables in tournament details comp

--- src/components/tournament-details/tournament-details-participants/TournamentDetailsParticipants.js
+++ src/components/tournament-details/tournament-details-participants/TournamentDetailsParticipants.js
@@ -4,6 +4,8 @@ import { CiSearch } from 'react-icons/ci'
 import { TiArrowSortedDown } from 'react-icons/ti'
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
 import { IoMdInformationCircleOutline } from "react-icons/io";
+import { RiGalleryView2 } from "react-icons/ri";
+import { IoGridOutline } from "react-icons/io5";
 import { LuUser, LuUsers2 } from "react-icons/lu";
 import { participantsList } from './participantsList'
 import profileStyles from "@/styles/profile/profile-page.module.css"
@@ -13,6 +15,7 @@ const TournamentDetailsParticipants = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [rowsPerPage, setRowsPerPage] = useState(10)
     const [searchQuery, setSearchQuery] = useState('')
+    const [showGalleryView, setShowGalleryView] = useState(false);
 
     const indexOfLastParticipant = currentPage * rowsPerPage
     const indexOfFirstParticipant = indexOfLastParticipant - rowsPerPage
@@ -59,6 +62,10 @@ const TournamentDetailsParticipants = () => {
         }
     }
 
+    const toggleTableView = () => {
+      setShowGalleryView(!showGalleryView)
+    }
+
   return (
 
     <div className={styles.tournamentDetailsParticipantsContainer}>
@@ -69,8 +76,22 @@ const TournamentDetailsParticipants = () => {
         <div className={styles.tournamentsEventsFilterSearchContainer}>
             <div className={styles.tournamentsEventsFilterContainer}>
                 <p className={styles.tournamentNumber}>{participantsList.length} participants</p>
+
+                <div className={styles.toggleTableView} onClick={toggleTableView}>
+                  {showGalleryView ? (
+                    <button className={styles.galleryViewBTN}>
+                      Less fields
+                      <RiGalleryView2 className={styles.galleryViewIcon} />
+                    </button>
+                  ) : (
+                    <button className={styles.galleryViewBTN}>
+                      All fields
+                      <IoGridOutline className={styles.galleryViewIcon} />
+                    </button>
+                  )}
+                </div>
             </div>
-          
+
             <div className={styles.searchContainer}>
                 <div className={styles.searchBar}>
                     <CiSearch 
@@ -89,57 +110,116 @@ const TournamentDetailsParticipants = () => {
             </div>
       </div>
 
-      <div className={`${styles.participantsTable}`}>
-        <div className={`${styles.gridHeader} ${profileStyles.middleLayerColor}`}>
-          <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Participant</div>
-          <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Type</div>
-          <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Ranking</div>
-          <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Location</div>
-          <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Tournaments</div>
-          <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Wins</div>
-          <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Losses</div>
-          <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Actions</div>
-        </div>
-
-        {currentParticipants.map((participant, index) => (
-          <div key={index} className={`${styles.gridRow} ${profileStyles.middleLayerColor}`}>
-            <div className={`${styles.gridItem} ${styles.nameColumn}`}>
-              <div className={styles.gameImageContainer}>
-                <Image
-                  src={participant.src}
-                  alt={participant.participant}
-                  className={styles.gameImage}
-                />
-              </div>
-              <p className={styles.gameName}>{participant.participant}</p>
-            </div>
-
-            <div className={`${styles.gridItem} ${styles.participantDiv}`}>
-              {participant.type === "Individual" ? (
-                <LuUser className={styles.participantIcon} />
-              ) : (
-                <LuUsers2 className={styles.participantIcon} />
-              )}
-              {participant.type}
-            </div>
-            
-            <div className={styles.gridItem}>{participant.ranking}</div>
-            <div className={styles.gridItem}>{participant.location}</div>
-            <div className={styles.gridItem}>{participant.tournaments}</div>
-
-            <div className={styles.gridItem}>{participant.wins}</div>
-            <div className={styles.gridItem}>{participant.losses}</div>
-            <div className={styles.gridItem}>
-              <button
-                className={`${styles.viewProfileBTN} ${profileStyles.topMostLayerColor}`}
-              >
-                View Profile
-              </button>
-            </div>
+      {!showGalleryView ? (
+        <div className={`${styles.participantsTable}`}>
+          <div className={`${styles.gridHeader} ${profileStyles.middleLayerColor}`}>
+            <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Participant</div>
+            <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Type</div>
+            <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Ranking</div>
+            <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Location</div>
+            <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Tournaments</div>
+            <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Wins</div>
+            <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Losses</div>
+            <div className={`${styles.gridItem} ${styles.gridItemHeader}`}>Actions</div>
           </div>
-        ))}
 
-      </div>
+          {currentParticipants.map((participant, index) => (
+            <div key={index} className={`${styles.gridRow} ${profileStyles.middleLayerColor}`}>
+              <div className={`${styles.gridItem} ${styles.nameColumn}`}>
+                <div className={styles.gameImageContainer}>
+                  <Image
+                    src={participant.src}
+                    alt={participant.participant}
+                    className={styles.gameImage}
+                  />
+                </div>
+                <p className={styles.gameName}>{participant.participant}</p>
+              </div>
+
+              <div className={`${styles.gridItem} ${styles.participantDiv}`}>
+                {participant.type === "Individual" ? (
+                  <LuUser className={styles.participantIcon} />
+                ) : (
+                  <LuUsers2 className={styles.participantIcon} />
+                )}
+                {participant.type}
+              </div>
+              
+              <div className={styles.gridItem}>{participant.ranking}</div>
+              <div className={styles.gridItem}>{participant.location}</div>
+              <div className={styles.gridItem}>{participant.tournaments}</div>
+
+              <div className={styles.gridItem}>{participant.wins}</div>
+              <div className={styles.gridItem}>{participant.losses}</div>
+              <div className={styles.gridItem}>
+                <button
+                  className={`${styles.viewProfileBTN} ${profileStyles.topMostLayerColor}`}
+                >
+                  View Profile
+                </button>
+              </div>
+            </div>
+          ))}
+
+        </div>
+        
+        ) : (
+        
+        <div className={styles.participantsTableExpandedContainer}>
+          <div className={`${styles.participantsTableExpanded}`}>
+            <div className={`${styles.gridHeaderExpanded} ${profileStyles.middleLayerColor}`}>
+              <div className={`${styles.gridItemExpanded} ${styles.gridItemHeaderExpanded}`}>Participant</div>
+              <div className={`${styles.gridItemExpanded} ${styles.gridItemHeaderExpanded}`}>Type</div>
+              <div className={`${styles.gridItemExpanded} ${styles.gridItemHeaderExpanded}`}>Ranking</div>
+              <div className={`${styles.gridItemExpanded} ${styles.gridItemHeaderExpanded}`}>Location</div>
+              <div className={`${styles.gridItemExpanded} ${styles.gridItemHeaderExpanded}`}>Tournaments</div>
+              <div className={`${styles.gridItemExpanded} ${styles.gridItemHeaderExpanded}`}>Wins</div>
+              <div className={`${styles.gridItemExpanded} ${styles.gridItemHeaderExpanded}`}>Losses</div>
+              <div className={`${styles.gridItemExpanded} ${styles.gridItemHeaderExpanded}`}>Actions</div>
+            </div>
+
+            {currentParticipants.map((participant, index) => (
+                <div key={index} className={`${styles.gridRowExpanded} ${profileStyles.middleLayerColor}`}>
+                  <div className={`${styles.gridItemExpanded} ${styles.nameColumn}`}>
+                    <div className={styles.gameImageContainer}>
+                      <Image
+                        src={participant.src}
+                        alt={participant.participant}
+                        className={styles.gameImage}
+                      />
+                    </div>
+                    <p className={styles.gameName}>{participant.participant}</p>
+                  </div>
+
+                  <div className={`${styles.gridItemExpanded} ${styles.participantDiv}`}>
+                    {participant.type === "Individual" ? (
+                      <LuUser className={styles.participantIcon} />
+                    ) : (
+                      <LuUsers2 className={styles.participantIcon} />
+                    )}
+                    {participant.type}
+                  </div>
+                
+                  <div className={styles.gridItemExpanded}>{participant.ranking}</div>
+                  <div className={styles.gridItemExpanded}>{participant.location}</div>
+                  <div className={styles.gridItemExpanded}>{participant.tournaments}</div>
+
+                  <div className={styles.gridItemExpanded}>{participant.wins}</div>
+                  <div className={styles.gridItemExpanded}>{participant.losses}</div>
+                  <div className={styles.gridItemExpanded}>
+                    <button
+                      className={`${styles.viewProfileBTN} ${profileStyles.topMostLayerColor}`}
+                    >
+                      View Profile
+                    </button>
+                  </div>
+                </div>
+              ))}
+
+          </div>
+        </div>
+      )}
+
 
       <div className={styles.paginationContainer}>
         <div className={styles.rowCountSelector}>
