1660a6e2a6411801a3fee1ab1fd818c05d915225
Otavie <otavieokuoyo@gmail.com>
2024-10-11 13:23:36 +0100

    Add template for Fortnite event section

--- src/components/events/all-events/fortnite-events/FortniteEvents.js
+++ src/components/events/all-events/fortnite-events/FortniteEvents.js
@@ -1,15 +1,13 @@
 import { useState } from 'react';
 import Image from 'next/image'
-import Link from 'next/link'
-import { AiOutlineTeam } from "react-icons/ai";
-import { LuGamepad2 } from "react-icons/lu";
+import Link from 'next/link';
 import { FiCalendar } from "react-icons/fi";
-import { GrTrophy } from "react-icons/gr";
 import { PiMoneyWavy } from "react-icons/pi";
 import { RiCopperCoinFill } from "react-icons/ri";
 import { LuArrowRight } from "react-icons/lu";
 import { LuArrowLeft } from "react-icons/lu";
 import { fortniteEventsList } from './fortniteEventsList'
+import upcomingEventsStyles from './../../upcoming-events/upcoming-events.module.css'
 import allEventsStyles from './../all-events.module.css'
 
 const FortniteEvents = () => {
@@ -35,53 +33,42 @@ const FortniteEvents = () => {
 
         <div className={allEventsStyles.cardsContainer}>
 
-            {fortniteEventsList.slice(0, showAll ? fortniteEventsList.length : 3).map((fifaTournament, index) => (
+            {fortniteEventsList.slice(0, showAll ? fortniteEventsList.length : 3).map((fortniteEvent, index) => (
             <div key={index} className={allEventsStyles.cardContainer}>
                 <div className={allEventsStyles.imageContainer}>
                     <Image
-                        src={fifaTournament.image.src}
-                        alt={fifaTournament.image.alt}
+                        src={fortniteEvent.src}
+                        alt={fortniteEvent.alt}
                     />
                 </div>
                             
-                <div className={`${allEventsStyles.descriptionContainer} ${allEventsStyles.descriptionContainer}`}>
-                    <div className={allEventsStyles.descriptionNameLocation}>
-                        <p><span className={allEventsStyles.descriptionName}>{fifaTournament.details.name}</span> - <span className={allEventsStyles.descriptionLocation}>{fifaTournament.details.location}</span></p>
+                <div className={`${upcomingEventsStyles.descriptionContainer} ${allEventsStyles.descriptionContainer}`}>
+                    <div className={upcomingEventsStyles.descriptionName}>
+                        <p><span className={upcomingEventsStyles.descriptionName}>{fortniteEvent.name}</span></p>
                     </div>
+
+                    <div className={upcomingEventsStyles.detailsContainer}>
+                        <div className={upcomingEventsStyles.eventTypeAndLocationContainer}>
+                            <p className={upcomingEventsStyles.eventTypeParagraph}>
+                                <span className={upcomingEventsStyles.eventTypeSpan}>{fortniteEvent.eventType}</span>
+                            </p>
+                        </div>
             
-                    <div className={allEventsStyles.teamsIndividualsContainer}>
-                        <p className={allEventsStyles.teamsIndividualsParagraph}>
-                            <span className={allEventsStyles.teamsIndividualsIconSpan}><AiOutlineTeam className={allEventsStyles.teamsIcon} /></span>
-                            <span className={allEventsStyles.teamsIndividualsSpan}>{fifaTournament.details.teamType}</span>
+                        <p className={upcomingEventsStyles.dateParagraph}>
+                            <span className={upcomingEventsStyles.calendarIconSpan}><FiCalendar className={upcomingEventsStyles.calendarIcon} /></span>
+                            <span className={upcomingEventsStyles.dateSpan}>{fortniteEvent.date}</span>
                         </p>
-                        <span className={allEventsStyles.playerNumber}># {fifaTournament.details.players}</span>
-                    </div>
-            
-                    <div className={`${allEventsStyles.nameDateContainer} ${allEventsStyles.nameDateContainer}`}>
-                        <p className={`${allEventsStyles.nameParagraph}`}>
-                            <span className={allEventsStyles.padIconSpan}><LuGamepad2 className={allEventsStyles.padIcon} /></span>
-                            <span className={allEventsStyles.nameSpan}>{fifaTournament.details.game}</span>
+                            
+                        <p className={upcomingEventsStyles.feeParagraph}>
+                            <span className={upcomingEventsStyles.feeIconSpan}><PiMoneyWavy className={upcomingEventsStyles.feeIcon} /></span>
+                            <span className={upcomingEventsStyles.feeSpan}>Fee: <span><RiCopperCoinFill className={upcomingEventsStyles.coinIcon} /></span> {fortniteEvent.fee}</span>
                         </p>
-                        <p className={`${allEventsStyles.dateParagraph}`}>
-                            <span className={allEventsStyles.calendarIconSpan}><FiCalendar className={allEventsStyles.calendarIcon} /></span>
-                            <span className={allEventsStyles.dateSpan}>{fifaTournament.details.date}</span>
-                        </p>
-                    </div>
-                                        
-                    <div className={`${allEventsStyles.prizeFeeContainer} ${allEventsStyles.prizeFeeContainer}`}>
-                        <p className={`${allEventsStyles.prizeParagraph}`}>
-                            <span className={allEventsStyles.prizeIconSpan}><GrTrophy className={allEventsStyles.prizeIcon} /></span>
-                            <span className={allEventsStyles.prizeSpan}>Prize: {fifaTournament.details.prize}</span>
-                        </p>
-                        <p className={`${allEventsStyles.feeParagraph}`}>
-                            <span className={allEventsStyles.feeIconSpan}><PiMoneyWavy className={allEventsStyles.feeIcon} /></span>
-                            <span className={allEventsStyles.feeSpan}>Fee: <span><RiCopperCoinFill className={allEventsStyles.coinIcon} /></span> {fifaTournament.details.fee}</span>
-                        </p>
+
                     </div>
                 
-                    <div className={`${allEventsStyles.buttonContainer} ${allEventsStyles.buttonContainer}`}>
-                        <Link href={'/tournament-details'} className={allEventsStyles.viewDetailsBTN}>View Details</Link>
-                        <Link href={'/tournament-register'} className={allEventsStyles.registerBTN}>Register</Link>
+                    <div className={`${upcomingEventsStyles.buttonContainer} ${allEventsStyles.buttonContainer}`}>
+                        <Link href={'/event-details'} className={upcomingEventsStyles.viewDetailsBTN}>View Details</Link>
+                        <Link href={'/event-register'} className={upcomingEventsStyles.registerBTN}>Register</Link>
                     </div>
                 </div>
                     
