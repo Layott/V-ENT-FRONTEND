c4ad4b8fa894c67944c047f651ca813eee6f6e6a
Otavie <otavieokuoyo@gmail.com>
2024-10-11 04:50:50 +0100

    Added template and some contents for the event page

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,29 +1,22 @@
 'use client'
 
-import Header from '@/components/header/Header'
-import Sidebar from '@/components/sidebar/Sidebar'
+import Header from '@/components/header/Header';
+import Sidebar from '@/components/sidebar/Sidebar';
+import EventsComponent from '@/components/events/EventsComponent';
 import styles from './events.module.css'
 
 const Events = () => {
+
   return (
     <div className={styles.pageContainer}>
-        <Header />
-
-        <main className={styles.mainContainer}>
-            <Sidebar />
-
-            <div className={styles.rightPaneContainer}>
-                <div className={styles.header}>
-                    <h3>Featured</h3>
-
-                    <div className={styles.searchFilterContainer}>
-                        Filter and Search
-                    </div>
-                </div>
-
-            </div>
-        </main>
+      <Header />
+      
+      <main className={styles.mainContainer}>
+        <Sidebar />
+        <EventsComponent />
         
+      </main>
+
     </div>
   )
 }
