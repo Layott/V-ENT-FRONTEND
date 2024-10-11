13811f9df0b40db1f5b63f2b61079ceabd9a8572
Otavie <otavieokuoyo@gmail.com>
2024-10-11 12:58:52 +0100

    Add template for PUBG event section

--- src/components/events/all-events/pubg-events/PUBGEvents.js
+++ src/components/events/all-events/pubg-events/PUBGEvents.js
@@ -1,15 +1,12 @@
 import { useState } from 'react';
 import Image from 'next/image'
 import Link from 'next/link';
-import { AiOutlineTeam } from "react-icons/ai";
-import { LuGamepad2 } from "react-icons/lu";
 import { FiCalendar } from "react-icons/fi";
-import { GrTrophy } from "react-icons/gr";
 import { PiMoneyWavy } from "react-icons/pi";
 import { RiCopperCoinFill } from "react-icons/ri";
 import { LuArrowRight } from "react-icons/lu";
 import { LuArrowLeft } from "react-icons/lu";
-import { pubgTournamentsList } from './pubgTournamentsList'
+import { pubgEventsList } from './pubgEventsList'
 import upcomingEventsStyles from './../../upcoming-events/upcoming-events.module.css'
 import allEventsStyles from './../all-events.module.css'
 
@@ -23,7 +20,7 @@ const PUBGEvents = () => {
   return (
     <div className={allEventsStyles.fifaTournamentsContainer}>
         <div className={allEventsStyles.header}>
-            <h3>PUBG Tournaments</h3>
+            <h3>PUBG Events</h3>
             {!showAll && (
             <button
                 className={allEventsStyles.seeMoreBTN}
@@ -36,53 +33,42 @@ const PUBGEvents = () => {
 
         <div className={allEventsStyles.cardsContainer}>
 
-            {pubgTournamentsList.slice(0, showAll ? pubgTournamentsList.length : 3).map((fifaTournament, index) => (
+            {pubgEventsList.slice(0, showAll ? pubgEventsList.length : 3).map((pubgEvent, index) => (
             <div key={index} className={allEventsStyles.cardContainer}>
                 <div className={allEventsStyles.imageContainer}>
                     <Image
-                        src={fifaTournament.image.src}
-                        alt={fifaTournament.image.alt}
+                        src={pubgEvent.src}
+                        alt={pubgEvent.alt}
                     />
                 </div>
                             
                 <div className={`${upcomingEventsStyles.descriptionContainer} ${allEventsStyles.descriptionContainer}`}>
-                    <div className={upcomingEventsStyles.descriptionNameLocation}>
-                        <p><span className={upcomingEventsStyles.descriptionName}>{fifaTournament.details.name}</span> - <span className={upcomingEventsStyles.descriptionLocation}>{fifaTournament.details.location}</span></p>
+                    <div className={upcomingEventsStyles.descriptionName}>
+                        <p><span className={upcomingEventsStyles.descriptionName}>{pubgEvent.name}</span></p>
                     </div>
+
+                    <div className={upcomingEventsStyles.detailsContainer}>
+                        <div className={upcomingEventsStyles.eventTypeAndLocationContainer}>
+                            <p className={upcomingEventsStyles.eventTypeParagraph}>
+                                <span className={upcomingEventsStyles.eventTypeSpan}>{pubgEvent.eventType}</span>
+                            </p>
+                        </div>
             
-                    <div className={upcomingEventsStyles.teamsIndividualsContainer}>
-                        <p className={upcomingEventsStyles.teamsIndividualsParagraph}>
-                            <span className={upcomingEventsStyles.teamsIndividualsIconSpan}><AiOutlineTeam className={upcomingEventsStyles.teamsIcon} /></span>
-                            <span className={upcomingEventsStyles.teamsIndividualsSpan}>{fifaTournament.details.teamType}</span>
-                        </p>
-                        <span className={upcomingEventsStyles.playerNumber}># {fifaTournament.details.players}</span>
-                    </div>
-            
-                    <div className={`${upcomingEventsStyles.nameDateContainer} ${allEventsStyles.nameDateContainer}`}>
-                        <p className={`${upcomingEventsStyles.nameParagraph}`}>
-                            <span className={upcomingEventsStyles.padIconSpan}><LuGamepad2 className={upcomingEventsStyles.padIcon} /></span>
-                            <span className={upcomingEventsStyles.nameSpan}>{fifaTournament.details.game}</span>
-                        </p>
-                        <p className={`${upcomingEventsStyles.dateParagraph}`}>
+                        <p className={upcomingEventsStyles.dateParagraph}>
                             <span className={upcomingEventsStyles.calendarIconSpan}><FiCalendar className={upcomingEventsStyles.calendarIcon} /></span>
-                            <span className={upcomingEventsStyles.dateSpan}>{fifaTournament.details.date}</span>
+                            <span className={upcomingEventsStyles.dateSpan}>{pubgEvent.date}</span>
                         </p>
-                    </div>
-                                        
-                    <div className={`${upcomingEventsStyles.prizeFeeContainer} ${allEventsStyles.prizeFeeContainer}`}>
-                        <p className={`${upcomingEventsStyles.prizeParagraph}`}>
-                            <span className={upcomingEventsStyles.prizeIconSpan}><GrTrophy className={upcomingEventsStyles.prizeIcon} /></span>
-                            <span className={upcomingEventsStyles.prizeSpan}>Prize: {fifaTournament.details.prize}</span>
-                        </p>
-                        <p className={`${upcomingEventsStyles.feeParagraph}`}>
+                            
+                        <p className={upcomingEventsStyles.feeParagraph}>
                             <span className={upcomingEventsStyles.feeIconSpan}><PiMoneyWavy className={upcomingEventsStyles.feeIcon} /></span>
-                            <span className={upcomingEventsStyles.feeSpan}>Fee: <span><RiCopperCoinFill className={upcomingEventsStyles.coinIcon} /></span> {fifaTournament.details.fee}</span>
+                            <span className={upcomingEventsStyles.feeSpan}>Fee: <span><RiCopperCoinFill className={upcomingEventsStyles.coinIcon} /></span> {pubgEvent.fee}</span>
                         </p>
+
                     </div>
                 
                     <div className={`${upcomingEventsStyles.buttonContainer} ${allEventsStyles.buttonContainer}`}>
-                        <Link href={'/tournament-details'} className={upcomingEventsStyles.viewDetailsBTN}>View Details</Link>
-                        <Link href={'/tournament-register'} className={upcomingEventsStyles.registerBTN}>Register</Link>
+                        <Link href={'/event-details'} className={upcomingEventsStyles.viewDetailsBTN}>View Details</Link>
+                        <Link href={'/event-register'} className={upcomingEventsStyles.registerBTN}>Register</Link>
                     </div>
                 </div>
                     
