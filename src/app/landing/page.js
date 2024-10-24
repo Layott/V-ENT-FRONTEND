484f108cacd8900d9478d319bbac36c6058a9855
Otavie <otavieokuoyo@gmail.com>
2024-10-25 00:54:01 +0100

    Add rough template for landing page

---
+++ src/app/landing/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './landing.module.css'
+
+const LandingPage = () => {
+  return (
+    <div className={styles.pageContainer}>
+        <h1>Landing Page</h1>
+        <p>Hello</p>
+    </div>
+  )
+}
+
+export default LandingPage
