c43d2a0a6f4e59c506a53663472fd8a374091c1e
Otavie <otavieokuoyo@gmail.com>
2024-10-27 16:06:37 +0100

    Add landing page hero section and the below hero section

---
+++ src/components/landing/left-hero/LeftHero.js
@@ -0,0 +1,27 @@
+import profileStyles from '@/styles/profile/profile-page.module.css'
+import styles from './left-hero.module.css'
+
+const LeftHero = () => {
+  return (
+    <div className={styles.leftHeroContainer}>
+        <div className={styles.leftHeroContainerCenter}>
+            <div className={styles.h1Container}>
+                <h1 className={styles.gaming}>Gaming</h1>
+                <h1 className={styles.anime}>Anime</h1>
+                <h1 className={styles.tribe}>Tribe</h1>
+            </div>
+            <div>
+                <p>
+                    Welcome to V-ENT, the ultimate platform where gaming, anime, and community converge. Whether you&#39;re a competitive esports player, a casual gamer, or an anime enthusiast, V-ENT offers tournaments, a vibrant marketplace, and unique features to help you immerse yourself in what you love most. Connect, compete, and engage in a community built for fans by fans. 
+                </p>
+            </div>
+            <div>
+                <input type="text" placeholder='Enter your email' />
+                <button className={`${profileStyles.waitlistBTN} ${styles.waitlistBTN}`}>Join the waitlist</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LeftHero
