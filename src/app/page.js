2f76cbd5429938e3fdec63c1a6e2ca245484e989
Otavie <otavieokuoyo@gmail.com>
2024-10-31 02:56:31 +0100

    Moved the landing page to the default url

--- src/app/page.js
+++ src/app/page.js
@@ -1,57 +1,83 @@
-'use client'
+// 'use client'
 
-import Header from '@/components/header/Header'
-import Sidebar from '@/components/sidebar/Sidebar'
+// import Header from '@/components/header/Header'
+// import Sidebar from '@/components/sidebar/Sidebar'
+// import styles from './page.module.css'
+// import { signOut, useSession } from 'next-auth/react'
+// import Landing from './landing/page'
+
+// const Page = () => {
+//   const { data: session } = useSession();
+
+//   // Access the session ID or session token
+//   if (session) {
+//     console.log("Session Data:", session); // Log the full session object to inspect what data is available
+//     const sessionId = session?.id || session?.sessionToken; // Adjust based on how session ID is stored
+//     // console.log("This is the Session ID/Token:", sessionId);
+//   }
+
+//   return (
+//     <>
+//       {session ? ( <Landing />) : (
+//         <>
+//           <div className={styles.pageContainer}>
+//             <Header />
+
+//             <main className={styles.mainContainer}>
+//               <Sidebar />
+
+//               <div className={styles.rightPaneContainer}>
+//                 <div className={styles.header}>
+//                   <h3>Home Page</h3>
+
+//                   <div className={styles.searchFilterContainer}>
+//                     Filter and Search
+//                   </div>
+//                 </div>
+
+//                 {/* Logout button */}
+//                 <div>
+//                   {/* <button
+//                     onClick={() => signOut()}
+//                     style={{ backgroundColor: 'red', padding: '1rem' }}
+//                   >
+//                     LOG OUT TEST
+//                   </button> */}
+//                 </div>
+//               </div>
+//             </main>
+//           </div>
+//         </>
+//       )}
+
+//     </>
+//   );
+// };
+
+// export default Page;
+
+
+
+import LandingHero from '@/components/landing/landing-hero/LandingHero'
+import BelowHero from '@/components/landing/below-hero/BelowHero'
+import TournamentsGamers from '@/components/landing/tournaments-gamers/TournamentsGamers'
+import ManageEvents from '@/components/landing/manage-events/ManageEvents'
 import styles from './page.module.css'
-import { signOut, useSession } from 'next-auth/react'
-import Landing from './landing/page'
 
-const Page = () => {
-  const { data: session } = useSession();
-
-  // Access the session ID or session token
-  if (session) {
-    console.log("Session Data:", session); // Log the full session object to inspect what data is available
-    const sessionId = session?.id || session?.sessionToken; // Adjust based on how session ID is stored
-    // console.log("This is the Session ID/Token:", sessionId);
-  }
-
+const LandingPage = () => {
   return (
-    <>
-      {session ? ( <Landing />) : (
-        <>
-          <div className={styles.pageContainer}>
-            <Header />
+    <div className={styles.pageContainer}>
 
-            <main className={styles.mainContainer}>
-              <Sidebar />
+        <div className={styles.heroContainer}>
+            <LandingHero />
+        </div>
 
-              <div className={styles.rightPaneContainer}>
-                <div className={styles.header}>
-                  <h3>Home Page</h3>
+        <BelowHero />
+        <TournamentsGamers />
+        <ManageEvents />
 
-                  <div className={styles.searchFilterContainer}>
-                    Filter and Search
-                  </div>
-                </div>
+    </div>
+  )
+}
 
-                {/* Logout button */}
-                <div>
-                  {/* <button
-                    onClick={() => signOut()}
-                    style={{ backgroundColor: 'red', padding: '1rem' }}
-                  >
-                    LOG OUT TEST
-                  </button> */}
-                </div>
-              </div>
-            </main>
-          </div>
-        </>
-      )}
-
-    </>
-  );
-};
-
-export default Page;
+export default LandingPage
