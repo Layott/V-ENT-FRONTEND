b8328639888a288d55f4724c45c9873481b96b24
Otavie <otavieokuoyo@gmail.com>
2024-11-01 21:24:58 +0100

    First draft of the footer section of landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,67 +1,8 @@
-// 'use client'
-
-// import Header from '@/components/header/Header'
-// import Sidebar from '@/components/sidebar/Sidebar'
-// import styles from './page.module.css'
-// import { signOut, useSession } from 'next-auth/react'
-// import Landing from './landing/page'
-
-// const Page = () => {
-//   const { data: session } = useSession();
-
-//   // Access the session ID or session token
-//   if (session) {
-//     console.log("Session Data:", session); // Log the full session object to inspect what data is available
-//     const sessionId = session?.id || session?.sessionToken; // Adjust based on how session ID is stored
-//     // console.log("This is the Session ID/Token:", sessionId);
-//   }
-
-//   return (
-//     <>
-//       {session ? ( <Landing />) : (
-//         <>
-//           <div className={styles.pageContainer}>
-//             <Header />
-
-//             <main className={styles.mainContainer}>
-//               <Sidebar />
-
-//               <div className={styles.rightPaneContainer}>
-//                 <div className={styles.header}>
-//                   <h3>Home Page</h3>
-
-//                   <div className={styles.searchFilterContainer}>
-//                     Filter and Search
-//                   </div>
-//                 </div>
-
-//                 {/* Logout button */}
-//                 <div>
-//                   {/* <button
-//                     onClick={() => signOut()}
-//                     style={{ backgroundColor: 'red', padding: '1rem' }}
-//                   >
-//                     LOG OUT TEST
-//                   </button> */}
-//                 </div>
-//               </div>
-//             </main>
-//           </div>
-//         </>
-//       )}
-
-//     </>
-//   );
-// };
-
-// export default Page;
-
-
-
 import LandingHero from '@/components/landing/landing-hero/LandingHero'
 import BelowHero from '@/components/landing/below-hero/BelowHero'
 import TournamentsGamers from '@/components/landing/tournaments-gamers/TournamentsGamers'
 import ManageEvents from '@/components/landing/manage-events/ManageEvents'
+import FooterLanding from '@/components/landing/footer-landing/FooterLanding'
 import styles from './page.module.css'
 
 const LandingPage = () => {
@@ -75,6 +16,7 @@ const LandingPage = () => {
         <BelowHero />
         <TournamentsGamers />
         <ManageEvents />
+        <FooterLanding />
 
     </div>
   )
