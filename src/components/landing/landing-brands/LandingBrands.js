7bfb7d7a71db8ff7f72c342f94604318c530b17b
Otavie <otavieokuoyo@gmail.com>
2024-11-03 17:24:56 +0100

    Add codes for the brand logo sections

---
+++ src/components/landing/landing-brands/LandingBrands.js
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import { brandLogos } from './BrandLogosList'
+import landingStyles from '@/styles/landing/landing.module.css'
+import styles from './landing-brands.module.css'
+
+const LandingBrands = () => {
+  return (
+    <div className={`${landingStyles.brandsContainer} ${styles.brandsContainer}`}>
+        <div className={`${landingStyles.innerBrandsContainer} ${styles.innerBrandsContainer}`}>
+            <div className={styles.brandHeader}>
+                <h1>Brands That Trust Us</h1>
+            </div>
+
+            <div className={styles.brandsLogoContainer}>
+                {brandLogos.map((brandLogo, index) => (
+                <div key={index} className={styles.brandLogoContainer}>
+                    <Image
+                        src={brandLogo.src}
+                        alt={brandLogo.alt}
+                    />
+                </div>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LandingBrands
