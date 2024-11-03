e2fef27e5e8f1e5326202e030034319339e55a1a
Otavie <otavieokuoyo@gmail.com>
2024-11-03 19:24:54 +0100

    Add the shopping section of the landing page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import LandingHero from '@/components/landing/landing-hero/LandingHero'
 import BelowHero from '@/components/landing/below-hero/BelowHero'
 import TournamentsGamers from '@/components/landing/tournaments-gamers/TournamentsGamers'
 import ManageEvents from '@/components/landing/manage-events/ManageEvents'
+import LandingShop from '@/components/landing/landing-shop/LandingShop'
 import LandingBrands from '@/components/landing/landing-brands/LandingBrands'
 import OtherFeatures from '@/components/landing/other-features/OtherFeatures'
 import JoinThousandsGamers from '@/components/landing/join-thousands-gamers/JoinThousandsGamers'
@@ -19,6 +20,7 @@ const LandingPage = () => {
         <BelowHero />
         <TournamentsGamers />
         <ManageEvents />
+        <LandingShop />
         <LandingBrands />
         <OtherFeatures />
         <JoinThousandsGamers />
