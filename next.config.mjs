a28d188b3e4907698265ad138ffc7dfbd8c8ea4f
Odusanya Oluwatimilehin <oluwapelumiodus@gmail.com>
2024-11-23 00:01:40 +0100

    configured some files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ['vermillionent.pythonanywhere.com'], 
+    },
+  };
+  
+  export default nextConfig;
+  
