2564018ff9ecb3987e6436a64d1e6ee5329a826a
Odusanya Oluwatimilehin <oluwapelumiodus@gmail.com>
2024-11-23 00:02:34 +0100

    updating APIs

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,13 +7,17 @@ export {handler as GET, handler as POST}
 
 
 export const VENT= {
+  WAITLIST: 'https://vermillionent.pythonanywhere.com/auth/add-email-to-waitlist/',
   SIGNUP: 'https://vermillionent.pythonanywhere.com/auth/signup/',
   USER_VERIFICATION: 'https://vermillionent.pythonanywhere.com/auth/get-username-with-email/',
   VERIFY: 'https://vermillionent.pythonanywhere.com/auth/get-user-status/',
   LOGIN: 'https://vermillionent.pythonanywhere.com/auth/login/',
-  FORGOT_PASSWORD:' ',
-  FORGOT_PASSWORD_TOKEN:' ',
-  RESET_PASSWORD:' ',
+  FORGOT_PASSWORD:'https://vermillionent.pythonanywhere.com/auth/forgot-password/send-token/',
+  FORGOT_PASSWORD_TOKEN:'https://vermillionent.pythonanywhere.com/auth/forgot-password/verify-token/',
+  RESET_PASSWORD:'',
   USER_PROFILE:'https://vermillionent.pythonanywhere.com/auth/get-user-informations/',
   EDIT_PROFILE:'https://vermillionent.pythonanywhere.com/auth/edit-profile-info/',
+  CREATE_EVENT:'https://vermillionent.pythonanywhere.com/event/create-event/',
+  GET_EVENTS:'https://vermillionent.pythonanywhere.com/event/get-all-events/',
 } 
+
