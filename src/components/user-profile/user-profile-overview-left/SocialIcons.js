6f1119dc66905e728e0aebbd8f15bf12ca4ed810
Otavie <otavieokuoyo@gmail.com>
2024-12-07 09:11:32 +0100

    Add codes and styles for empty profile pt 1"

---
+++ src/components/user-profile/user-profile-overview-left/SocialIcons.js
@@ -0,0 +1,30 @@
+import { 
+    FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaPinterest, FaSnapchat, FaReddit, FaLinkedin, FaTiktok, 
+    FaTwitch, FaGithub, FaDiscord, FaDribbble, FaSpotify, FaSlack, FaMedium, FaVimeo, FaTumblr, FaBehance, FaGoogle 
+} from "react-icons/fa";
+import { FaGlobe } from "react-icons/fa6"; 
+import profileStyles from "@/styles/profile/profile-page.module.css";
+
+export const socialIcons = {
+    facebook: <FaFacebook className={profileStyles.socialIcon} />,
+    twitter: <FaTwitter className={profileStyles.socialIcon} />,
+    instagram: <FaInstagram className={profileStyles.socialIcon} />,
+    youtube: <FaYoutube className={profileStyles.socialIcon} />,
+    pinterest: <FaPinterest className={profileStyles.socialIcon} />,
+    snapchat: <FaSnapchat className={profileStyles.socialIcon} />,
+    reddit: <FaReddit className={profileStyles.socialIcon} />,
+    linkedin: <FaLinkedin className={profileStyles.socialIcon} />,
+    tiktok: <FaTiktok className={profileStyles.socialIcon} />,
+    twitch: <FaTwitch className={profileStyles.socialIcon} />,
+    github: <FaGithub className={profileStyles.socialIcon} />,
+    discord: <FaDiscord className={profileStyles.socialIcon} />,
+    dribbble: <FaDribbble className={profileStyles.socialIcon} />,
+    spotify: <FaSpotify className={profileStyles.socialIcon} />,
+    slack: <FaSlack className={profileStyles.socialIcon} />,
+    medium: <FaMedium className={profileStyles.socialIcon} />,
+    vimeo: <FaVimeo className={profileStyles.socialIcon} />,
+    tumblr: <FaTumblr className={profileStyles.socialIcon} />,
+    behance: <FaBehance className={profileStyles.socialIcon} />,
+    google: <FaGoogle className={profileStyles.socialIcon} />,
+    default: <FaGlobe className={profileStyles.socialIcon} />, 
+};
--- src/components/user-profile/user-profile-overview-left/UserProfileOverviewLeft.js
+++ src/components/user-profile/user-profile-overview-left/UserProfileOverviewLeft.js
@@ -1,50 +1,20 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import playStation from '@/images/playstation.webp';
-import xBox from '@/images/Xbox_logo.webp';
-import godOfWar from '@/images/god_of_war.webp';
-import fc25 from '@/images/fc25.webp';
-import steam from '@/images/steam.webp';
-import epicGames from '@/images/EpicGames.webp';
-import nintendoSwitch from '@/images/NintendoSwitch.webp';
-import {
-  FaFacebook,
-  FaInstagram,
-  FaYoutube,
-  FaArrowLeft,
-  FaGlobe,
-} from "react-icons/fa";
-import { FaXTwitter, FaGithub } from "react-icons/fa6";
+import { FaArrowLeft } from "react-icons/fa";
+import { socialIcons } from './SocialIcons';
 import { HiPlus } from "react-icons/hi";
 import { MdDelete } from "react-icons/md";
 import profileStyles from "@/styles/profile/profile-page.module.css";
 
-const UserProfileOverviewLeft = ({ interest, socialLinks }) => {
+const UserProfileOverviewLeft = ({ interests = [], gamingAccounts = [], socialLinks }) => {
   const [showMoreInterests, setShowMoreInterests] = useState(false);
   const [showMoreGamingAccounts, setShowMoreGamingAccounts] = useState(false);
-
-  // Gaming Accounts Data
-  const gamingAccountsList = [
-    { logo: playStation, name: 'PlayStation', handle: '@frostbite_psn' },
-    { logo: xBox, name: 'XBox', handle: '@frostbite_xbox' },
-    { logo: godOfWar, name: 'God of War', handle: '@frostbite_gow' },
-    { logo: fc25, name: 'FC25', handle: '@frostbiteFC' },
-    { logo: steam, name: 'Steam', handle: '@frostbite_steam' },
-    { logo: epicGames, name: 'Epic Games', handle: '@frostbite_eg' },
-    { logo: nintendoSwitch, name: 'Nintendo Switch', handle: '@frostbite_ns' },
-  ];
+  const [showMoreSocials, setShowMoreSocials] = useState(false);
 
   // Social Icon Mapper
   const getSocialIcon = (title) => {
-    const icons = {
-      facebook: <FaFacebook className={profileStyles.socialIcon} />,
-      twitter: <FaXTwitter className={profileStyles.socialIcon} />,
-      instagram: <FaInstagram className={profileStyles.socialIcon} />,
-      youtube: <FaYoutube className={profileStyles.socialIcon} />,
-      github: <FaGithub className={profileStyles.socialIcon} />,
-    };
-    return icons[title.toLowerCase()] || <FaGlobe className={profileStyles.socialIcon} />;
+    return socialIcons[title.toLowerCase()] || socialIcons.default;
   };
 
   // Deleting/Remove Gaming Accounts
@@ -57,25 +27,47 @@ const UserProfileOverviewLeft = ({ interest, socialLinks }) => {
       {/* Interests Section */}
       <div className={profileStyles.sectionContainer}>
         <h4 className={profileStyles.profileH4Header}>Interests</h4>
+
         <div className={profileStyles.interestsListContainer}>
-          {interest.slice(0, showMoreInterests ? interest.length : 9).map((interest, index) => (
-            <span key={index} className={`${profileStyles.interest} ${profileStyles.topMostLayerColor}`}>
-              {interest}
-            </span>
-          ))}
-          <button
-            onClick={() => setShowMoreInterests((prev) => !prev)}
-            className={`${profileStyles.topMostLayerColor} ${profileStyles.showMoreBTN}`}
-          >
-            {showMoreInterests ? (
-              <>
-                <FaArrowLeft className={profileStyles.rightOrLeftArrowIcon} /> See less
-              </>
+
+            {interests.length === 0 ? (
+                <div className={profileStyles.noInterestsContainer}>
+                    <h4 className={profileStyles.profileH4HeaderEmptyContent}>No interests yet</h4>
+                    <p className={profileStyles.emptyParagraphContent}>You haven&#39;t added any of your interests yet</p>
+
+                    <Link href={'/edit-user-profile'} className={`${profileStyles.addInterestsBTN} ${profileStyles.topMostLayerColor}`}>
+                        <HiPlus className={profileStyles.profileH4IconsEmptyContent} /> Add Interests
+                    </Link>
+                </div>
             ) : (
-              `See more +${interest.length - 9}`
+                <>
+                    {interests.slice(0, showMoreInterests ? interests.length : 9).map((interest, index) => (
+                        <span
+                            key={index}
+                            className={`${profileStyles.interest} ${profileStyles.topMostLayerColor}`}
+                        >
+                            {interest}
+                        </span>
+                    ))}
+
+                    {interests.length > 9 && (
+                        <button
+                            onClick={() => setShowMoreInterests((prev) => !prev)}
+                            className={`${profileStyles.topMostLayerColor} ${profileStyles.showMoreBTN}`}
+                        >
+                            {showMoreInterests ? (
+                                <>
+                                    <FaArrowLeft className={profileStyles.rightOrLeftArrowIcon} /> See less
+                                </>
+                            ) : (
+                                `See more +${interests.length - 9}`
+                            )}
+                        </button>
+                    )}
+                </>
             )}
-          </button>
         </div>
+
       </div>
 
       <hr className={profileStyles.sectionHr} />
@@ -88,44 +80,63 @@ const UserProfileOverviewLeft = ({ interest, socialLinks }) => {
             <HiPlus className={profileStyles.profileH4Icons} /> Add
           </button>
         </div>
+
         <div className={profileStyles.gamingAccountsListContainer}>
-          {gamingAccountsList
-            .slice(0, showMoreGamingAccounts ? gamingAccountsList.length : 4)
-            .map((account, index) => (
-              <div key={index} className={profileStyles.gamingAccount}>
-                <div>
-                  <div className={profileStyles.gameLogoAndName}>
-                    <div className={profileStyles.gameLogo}>
-                      <Image src={account.logo} alt={account.name} />
-                    </div>
-                    <div className={profileStyles.gameName}>
-                      <h4>{account.name}</h4>
-                      <p>{account.handle}</p>
-                    </div>
-                  </div>
+            {gamingAccounts.length === 0 ? (
+                <div className={profileStyles.noGamingAccountsListContainer}>
+                    <h4 className={profileStyles.profileH4HeaderEmptyContent}>No accounts connected yet</h4>
+                    <p className={profileStyles.emptyParagraphContent}>You haven&#39;t connected any of your accounts yet</p>
+
+                    <Link href={'/edit-user-profile'} className={`${profileStyles.addInterestsBTN} ${profileStyles.topMostLayerColor}`}>
+                        <HiPlus className={profileStyles.profileH4IconsEmptyContent} /> Connect Accounts 
+                    </Link>
                 </div>
-                <div
-                  className={`${profileStyles.topMostLayerColor} ${profileStyles.gamingAccountIcon}`}
-                  onClick={() => deleteGamingAccount(index)}
-                >
-                  <MdDelete className={profileStyles.profileH4Icons} />
-                </div>
-              </div>
-            ))}
+                ) : (
+                    <>
+                        {gamingAccounts.slice(0, showMoreGamingAccounts ? gamingAccounts.length : 4).map((account, index) => (
+                            <div key={index} className={profileStyles.gamingAccount}>
+                                <div>
+                                    <div className={profileStyles.gameLogoAndName}>
+                                        <div className={profileStyles.gameLogo}>
+                                            <Image
+                                                src={account.logo}
+                                                alt={account.name}
+                                            />
+                                        </div>
+                                        <div className={profileStyles.gameName}>
+                                            <h4>{account.name}</h4>
+                                            <p>{account.handle}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div
+                                    className={`${profileStyles.topMostLayerColor} ${profileStyles.gamingAccountIcon}`}
+                                    onClick={() => deleteGamingAccount(index)}
+                                >
+                                    <MdDelete className={profileStyles.profileH4Icons} />
+                                </div>
+                            </div>
+                        ))}
+
+                        {gamingAccounts.length > 4 && (
+                            <button
+                                onClick={() => setShowMoreGamingAccounts(prev => !prev)}
+                                className={`${profileStyles.topMostLayerColor} ${profileStyles.seeMoreAccountsBTN}`}
+                            >
+                                {showMoreGamingAccounts ? (
+                                    <>
+                                        <FaArrowLeft className={profileStyles.rightOrLeftArrowIcon} /> See less
+                                    </>
+                                ) : (
+                                    `See more +${gamingAccounts.length - 4}`
+                                )}
+                            </button>                        
+                        )}
+                    </>
+                )
+            }
         </div>
 
-        <button
-          onClick={() => setShowMoreGamingAccounts((prev) => !prev)}
-          className={`${profileStyles.topMostLayerColor} ${profileStyles.seeMoreAccountsBTN}`}
-        >
-          {showMoreGamingAccounts ? (
-            <>
-              <FaArrowLeft className={profileStyles.rightOrLeftArrowIcon} /> See less
-            </>
-          ) : (
-            `See more +${gamingAccountsList.length - 4}`
-          )}
-        </button>
       </div>
 
       <hr className={profileStyles.sectionHr} />
@@ -134,16 +145,45 @@ const UserProfileOverviewLeft = ({ interest, socialLinks }) => {
       <div className={profileStyles.sectionContainer}>
         <h4 className={profileStyles.sectionHeader}>Social Links</h4>
         <div className={profileStyles.socialLinksListContainer}>
-          {socialLinks.map((link, index) => (
-            <Link
-              href={link.url}
-              key={index}
-              target="_blank"
-              className={`${profileStyles.socialLink} ${profileStyles.topMostLayerColor}`}
-            >
-              {getSocialIcon(link.title)} {link.title}
-            </Link>
-          ))}
+            {socialLinks.length === 0 ? (
+                <div className={profileStyles.noInterestsContainer}>
+                    <h4 className={profileStyles.profileH4HeaderEmptyContent}>No social links yet</h4>
+                    <p className={profileStyles.emptyParagraphContent}>You haven&#39;t connected any social media links yet</p>
+
+                    <Link href={'/edit-user-profile'} className={`${profileStyles.addInterestsBTN} ${profileStyles.topMostLayerColor}`}>
+                        <HiPlus className={profileStyles.profileH4IconsEmptyContent} /> Add Social Links
+                    </Link>
+                </div>
+            ) : (
+                <>
+                    {socialLinks.slice(0, showMoreSocials ? socialLinks.length : 4).map((socialLink, index) => (
+                        <Link
+                            href={socialLink.url}
+                            key={index}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className={`${profileStyles.interest} ${profileStyles.topMostLayerColor}`}
+                        >
+                            {getSocialIcon(socialLink.title)} {socialLink.title}
+                        </Link>
+                    ))}
+
+                    {socialLinks.length > 4 && (
+                        <button
+                            onClick={() => setShowMoreSocials((prev) => !prev)}
+                            className={`${profileStyles.topMostLayerColor} ${profileStyles.showMoreBTN}`}
+                        >
+                            {showMoreSocials ? (
+                                <>
+                                    <FaArrowLeft className={profileStyles.rightOrLeftArrowIcon} /> See less
+                                </>
+                            ) : (
+                                `See more +${socialLinks.length - 4}`
+                            )}
+                        </button>
+                    )}
+                </>
+            )}
         </div>
       </div>
     </div>
