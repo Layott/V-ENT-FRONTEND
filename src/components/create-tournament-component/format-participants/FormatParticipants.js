f194f0261f036ff122fcd621c3d4d2837de20779
Otavie <otavieokuoyo@gmail.com>
2024-12-20 20:53:54 +0100

    Add proceed, back and publish btns

--- src/components/create-tournament-component/format-participants/FormatParticipants.js
+++ src/components/create-tournament-component/format-participants/FormatParticipants.js
@@ -1,10 +1,18 @@
-import { IoMdArrowForward  } from "react-icons/io";
+import { IoMdArrowForward, IoMdArrowBack } from "react-icons/io";
 import TournamentFormat from "./tournament-format/TournamentFormat";
 import Participants from "./participants/Participants";
 import TournamentRules from "./tournament-rules/TournamentRules";
 import createTournamentStyles from '@/styles/create-tournament/create-tournament.module.css'
 
-const FormatParticipants = () => {
+const FormatParticipants = ({ setSelectedTab }) => {
+  const handleProceed = () => {
+    setSelectedTab((prevTab) => prevTab + 1);
+  }
+
+  const handleBack = () => {
+    setSelectedTab((prevTab) => prevTab - 1);
+  }
+
   return (
     <div className={createTournamentStyles.generalTabContainer}>
         <header className={createTournamentStyles.createTournamentHeader}>
@@ -24,12 +32,23 @@ const FormatParticipants = () => {
             Save Draft
           </button>
           
-          <button
-            className={`${createTournamentStyles.btn} ${createTournamentStyles.proceedBTN}`}
-          >
-            Proceed
-            <IoMdArrowForward className={createTournamentStyles.forwardArrowIcon} />
-          </button>
+          <div className={createTournamentStyles.backAndProceedContainer}>
+            <button
+              className={`${createTournamentStyles.btn} ${createTournamentStyles.backBTN}`}
+              onClick={handleBack}
+            >
+              <IoMdArrowBack className={createTournamentStyles.backArrowIcon} />
+              Back
+            </button>
+
+            <button
+              className={`${createTournamentStyles.btn} ${createTournamentStyles.proceedBTN}`}
+              onClick={handleProceed}
+            >
+              Proceed
+              <IoMdArrowForward className={createTournamentStyles.forwardArrowIcon} />
+            </button>
+          </div>
         </div>
     </div>
   )
