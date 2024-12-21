6c8ecb2a9365647f720ced2909143ef7833f9943
Otavie <otavieokuoyo@gmail.com>
2024-12-21 12:08:43 +0100

    Add codes for the review sections components

--- src/components/create-tournament-component/review/Review.js
+++ src/components/create-tournament-component/review/Review.js
@@ -1,5 +1,11 @@
-import { IoMdArrowForward, IoMdArrowBack } from "react-icons/io";
+import { IoMdArrowBack } from "react-icons/io";
+import ReviewHeaderComponent from "@/components/create-tournament-component/review/review-header-component/ReviewHeaderComponent";
+import ReviewBasicInfo from "@/components/create-tournament-component/review/review-basic-info/ReviewBasicInfo";
+import ReviewFormatParticipants from "@/components/create-tournament-component/review/review-format-participants/ReviewFormatParticipants";
+import ReviewPrizeDistribution from "@/components/create-tournament-component/review/review-prize-distribution/ReviewPrizeDistribution";
+import ReviewSponsorLinks from "./review-sponsor-links/ReviewSponsorLinks";
 import createTournamentStyles from '@/styles/create-tournament/create-tournament.module.css'
+import styles from './review.module.css'
 
 const Review = ({ setSelectedTab }) => {
   const handleBack = () => {
@@ -7,14 +13,50 @@ const Review = ({ setSelectedTab }) => {
   }
 
   const handlePublish = () => {
-    // setSelectedTab((prevTab) => prevTab - 1);
+    // Code to Handle Publish
   }
 
   return (
-    <div className={createTournamentStyles.generalTabContainer}>
+    <div className={`${createTournamentStyles.generalTabContainer} ${styles.generalTabContainer}`}>
       <header>
           <h1>Review</h1>
       </header>
+
+      <ReviewHeaderComponent
+        title="Basic Info"
+        isCompleted={true}
+        editTabIndex={1}
+        setSelectedTab={setSelectedTab}
+      >
+        <ReviewBasicInfo />
+      </ReviewHeaderComponent>
+
+      <ReviewHeaderComponent
+        title="Format & Participants"
+        isCompleted={false}
+        editTabIndex={2}
+        setSelectedTab={setSelectedTab}
+      >
+        <ReviewFormatParticipants />
+      </ReviewHeaderComponent>
+
+      <ReviewHeaderComponent
+        title="Prize Distribution"
+        isCompleted={false}
+        editTabIndex={3}
+        setSelectedTab={setSelectedTab}
+      >
+        <ReviewPrizeDistribution />
+      </ReviewHeaderComponent>
+
+      <ReviewHeaderComponent
+        title="Sponsors & Links"
+        isCompleted={true}
+        editTabIndex={4}
+        setSelectedTab={setSelectedTab}
+      >
+        <ReviewSponsorLinks />
+      </ReviewHeaderComponent>
 
       <div className={createTournamentStyles.buttonContainer}>
         <button
