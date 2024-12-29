6d6da2332a3cf0c27915a37d63336d6e0f3eca6a
Otavie <otavieokuoyo@gmail.com>
2024-12-29 17:50:33 +0100

    Add boiler plate code for mobile menu

--- src/app/tournaments/create-tournament/page.js
+++ src/app/tournaments/create-tournament/page.js
@@ -2,6 +2,7 @@
 
 import Header from '@/components/header/Header';
 import Sidebar from '@/components/sidebar/Sidebar';
+// import MobileSidebar from '@/components/mobile-sidebar/MobileSidebar';
 import CreateTournamentComponent from '@/components/create-tournament-component/CreateTournamentComponent';
 import styles from './create-tournament.module.css';
 
@@ -12,6 +13,7 @@ const TournamentCreation = () => {
 
         <main className={styles.mainContainer}>
           <Sidebar />
+          {/* <MobileSidebar /> */}
           <CreateTournamentComponent />
 
         </main>
---
+++ src/components/mobile-sidebar/MobileSidebar.js
@@ -0,0 +1,103 @@
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+import { FiHome, FiSettings } from "react-icons/fi";
+import { MdOutlineEvent } from "react-icons/md";
+import { FaTrophy, FaUsers, FaShoppingCart, FaTv } from 'react-icons/fa';
+import { PiRankingBold } from "react-icons/pi";
+import { IoWalletOutline } from "react-icons/io5";
+import { FiShoppingBag } from "react-icons/fi";
+import logoRed from "@/images/logo_mark_red.svg"
+import styles from './mobile-sidebar.module.css'
+
+const MobileSidebar = () => {
+    const pathname = usePathname()      // Gets the current pathname
+
+    // Function to Check if the Route is Active
+    const isActive = (href) => pathname === href
+
+  return (
+    <div className={styles.desktopSidebar}>
+        <div className={styles.logoContainer}>
+            <Link className={styles.logoLink} href={'/'}>
+                <div className={styles.innerLogoContainer}>
+                    <Image
+                        src={logoRed}
+                        alt='Logo'
+                        className={styles.logo}
+                    />
+                </div>
+                <h1>v-ent</h1>
+            </Link>
+        </div>
+
+        <nav className={styles.sidebarNav}>
+            <ul className={styles.sidebarList}>
+                <li className={`${styles.sidebarItem} ${isActive('/') ? styles.activeLink : ''}`}>
+                    <Link href={'/'} className={styles.iconTextLink}>
+                        <FiHome className={styles.sidebarIcon} /> Home
+                    </Link>
+                </li>
+
+                <li className={`${styles.sidebarItem} ${isActive('/tournaments') ? styles.activeLink : ''}`}>
+                    <Link href={'/tournaments'} className={styles.iconTextLink}>
+                        <FaTrophy className={styles.sidebarIcon} /> Tournaments
+                    </Link>
+                </li>
+
+                <li className={`${styles.sidebarItem} ${isActive('/events') ? styles.activeLink : ''}`}>
+                    <Link href={'/events'} className={styles.iconTextLink}>
+                        <MdOutlineEvent className={styles.sidebarIcon} /> Events
+                    </Link>
+                </li>
+                
+                <li className={`${styles.sidebarItem} ${isActive('/anime') ? styles.activeLink : ''}`}>
+                    <Link href={'/anime'} className={styles.iconTextLink}>
+                        <FaTv className={styles.sidebarIcon} /> Anime
+                    </Link>
+                </li>
+                
+                <li className={`${styles.sidebarItem} ${isActive('/rankings') ? styles.activeLink : ''}`}>
+                    <Link href={'/rankings'} className={styles.iconTextLink}>
+                        <PiRankingBold className={styles.sidebarIcon} /> Rankings
+                    </Link>
+                </li>
+                
+                <li className={`${styles.sidebarItem} ${isActive('/teams') ? styles.activeLink : ''}`}>
+                    <Link href={'/teams'} className={styles.iconTextLink}>
+                        <FaUsers className={styles.sidebarIcon} /> Teams
+                    </Link>
+                </li>
+
+                <li className={`${styles.sidebarItem} ${isActive('/wallets') ? styles.activeLink : ''}`}>
+                    <Link href={'/wallets'} className={styles.iconTextLink}>
+                        <IoWalletOutline className={styles.sidebarIcon} /> Wallets
+                    </Link>
+                </li>
+                            
+                <li className={styles.sidebarItem}>
+                    <span className={styles.disabledLink}>
+                        <span className={styles.iconMarketplaceSpan}><FaShoppingCart className={styles.sidebarIcon} /> Marketplace</span>
+                        <span className={styles.comingSoon}>Coming Soon</span>
+                    </span>
+                </li>
+            
+                <li className={styles.sidebarItem}>
+                    <span className={styles.disabledLink}>
+                        <span className={styles.iconShopSpan}><FiShoppingBag className={styles.sidebarIcon} /> Shop</span>
+                        <span className={styles.comingSoon}>Coming Soon</span>
+                    </span>
+                </li>
+            
+                <li className={`${styles.sidebarItem} ${isActive('/settings') ? styles.activeLink : ''}`}>
+                    <Link href={'/settings'} className={styles.iconTextLink}>
+                        <FiSettings className={styles.sidebarIcon} /> Settings
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default MobileSidebar
